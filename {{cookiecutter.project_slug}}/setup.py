b95705742d252f98fb51ac8ce983b0a87e53c3ba
dem4ply <dem4ply@gmail.com>
2019-11-22 11:54:31 -0600

    add licence

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -18,7 +18,8 @@ requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'C
     'BSD license': 'License :: OSI Approved :: BSD License',
     'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
     'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
-    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'WTFPL': 'License :: Public Domain'
 } %}
 
 setup(
