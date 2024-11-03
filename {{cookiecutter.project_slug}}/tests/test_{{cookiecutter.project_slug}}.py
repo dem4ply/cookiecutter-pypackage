94f387c42936c91576e891d7a972ba0702afc084
dem4ply <dem4ply@gmail.com>
2024-11-02 20:03:09 -0600

    update

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,53 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-{% if cookiecutter.use_pytest == 'y' -%}
-import pytest
-{% else %}
 import unittest
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from click.testing import CliRunner
-{%- endif %}
-
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from {{ cookiecutter.project_slug }} import cli
-{%- endif %}
-
-{%- if cookiecutter.use_pytest == 'y' %}
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
-{%- else %}
-
-
-class Test_{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+class Test_{{ cookiecutter.project_slug }}( unittest.TestCase ):
     def setUp(self):
         pass
 
@@ -56,16 +12,3 @@ class Test_{{ cookiecutter.project_slug|title }}(unittest.TestCase):
 
     def test_should_work(self):
         pass
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
-{%- endif %}
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -3,19 +3,8 @@
 import argparse
 {%- endif %}
 import sys
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-import click
-{%- endif %}
 
-{% if cookiecutter.command_line_interface|lower == 'click' %}
-@click.command()
-def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-{%- endif %}
+
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 def main():
     """Console script for {{cookiecutter.project_slug}}."""
